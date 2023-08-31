a961c699cd54446ea6862c308bf85e8c05caa1d0
washucode <estherw8525@gmail.com>
2023-08-31 23:09:50 +0300

    task 18 correction

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -10,21 +10,14 @@
 
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
-	binary_tree_t *parent = node->parent;
-	binary_tree_t *grandparent = parent->parent;
-
-	/* If node is NULL or the parent is NULL, return NULL */
-	if (node == NULL || node->parent == NULL)
-		return (NULL);
-
 	
-
-	/* If the grandparent is NULL, there is no uncle */
-	if (grandparent == NULL)
+	/* If node is NULL or the parent is NULL, return NULL */
+	if (node == NULL || node->parent == NULL || node->parent->parent == NULL)
 		return (NULL);
+
 	/* If the parent is the left child, return the right child */
-	if (grandparent->left == parent)
-		return (grandparent->right);
+	if (node->parent->parent->left == node->parent)
+		return (node->parent->parent->right);
 	/* If the parent is the right child, return the left child */
-	return (grandparent->left);
+	return (node->parent->parent->left);
 }
